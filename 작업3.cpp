c48e237e37cc51a6d6eee37517353f1351ceb1bd
junsu268 <junsu09259@gmail.com>
2025-04-08 18:39:50 +0900

    Add files via upload

---
+++ 작업3.cpp
@@ -0,0 +1,7 @@
+ #include <stdio.h>
+  int main()
+  {
+  	 char c = 't';
+  	 printf("%c",c);
+  	  return 0;
+  }
---
+++ 작업4.cpp
@@ -0,0 +1,7 @@
+ #include <stdio.h>
+  int main()
+  {
+     int a;
+  	 scan("%d", &a);// 3입력
+     printf("%", a);
+  }
